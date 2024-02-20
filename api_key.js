733247231611a12e55005197816f3b5b544f6c3e
unknown <favoriteinfinite@gmail.com>
2024-02-20 23:24:41 +0800

    新增先前試做的api串接2;304

---
+++ api_key.js
@@ -0,0 +1,2 @@
+let popularMovie='https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=zh-TW&page=1&sort_by=popularity.desc&api_key=a053ccf78d42e2c3225f9ca7d5af331c';
+let apiKey='&api_key=a053ccf78d42e2c3225f9ca7d5af331c'
