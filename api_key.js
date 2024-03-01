c538a126da2758aedeee20ae460c91d58767c87c
rudy870103 <favoriteinfinite@gmail.com>
2024-03-01 13:18:51 +0800

    把近期熱映的popular movie改成trending movie的api

--- api_key.js
+++ api_key.js
@@ -1,2 +1,3 @@
 let popularMovie='https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=zh-TW&page=1&sort_by=popularity.desc&api_key=a053ccf78d42e2c3225f9ca7d5af331c';
+let trendingMovie='https://api.themoviedb.org/3/trending/movie/week?language=zh-TW&api_key=a053ccf78d42e2c3225f9ca7d5af331c'
 let apiKey='&api_key=a053ccf78d42e2c3225f9ca7d5af331c'
